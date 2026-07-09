a68eec3f2f6846cdfe346a07e554821f06e48a24
manlymaia42 <135257352+manlymaia42@users.noreply.github.com>
2026-07-09 09:33:33 -0300

    qualq

--- perguntas.js
+++ perguntas.js
@@ -2,141 +2,140 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    ' portugues
+     que é uma oxítona? 
+     'É uma palavra cuja última sílaba é a mais forte (ex: café, amor).''
+
+criaCartao(
+    'portugues',
+    'Qual a função do adjetivo?? 
+    ' Caracterizar ou qualificar um substantivo (ex: casa bonita).'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'matematica',
+    'O que é a hipotenusa? ',
+    'É o lado mais longo de um triângulo retângulo, oposto ao ângulo de $90^\circ$.'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'matematica',
+    'Quanato vale a soma dos ângulos internos de um triângulo?',
+    'Sempre vale $180^\circ$.'
 )
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    '
+    historia',
+    'Quem foi o primeiro presidente do Brasil?', o conjun
+    'Marechal Deodoro da Fonseca.'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'historia',
+    'Em que ano teve início a Primeira Guerra Mundial?',
+    'Em 1914..'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra.'
 )
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'geografia',
+    'O que são linhas imaginárias horizontais que circundam a Terra chamadas?',
+    'Paralelos (medem a latitude)'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'biologia',
+    'Qual é a principal função dos glóbulos vermelhos no sangue?',
+    'Transportar oxigênio para as células do corpo.'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'biologia',
+    'Qual organela celular é responsável pela produção de energia (respiração celular)?',
+    'A mitocôndria.'
 )
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'quimica,
+    'Qual é o elemento químico mais abundante no universo?',
+    'O hidrogênio ($H$).'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'quimica,
+    O que determina o número atômico de um elemento?
+    'A quantidade de prótons que ele possui no núcleo.
+
+criaCartao(
+    'fisica',
+    'Qual é a unidade de medida da força no Sistema Internacional (SI)?',
+    'O Newton ($N$).'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'fisica',
+    'O que diz a Primeira Lei de Newton (Lei da Inércia)?',
+    'Que um corpo tende a permanecer em seu estado de repouso ou de movimento retilíneo uniforme, a menos que uma força externa atue sobre ele´
 )
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'literatura',
+    'Quem é o autor da obra "Dom Casmurro"?
+     ´Machado de Assis.´',
+    
+
+criaCartao(
+    ' literatura',
+     Qual movimento literário brasileiro tinha como marco inicial a publicação de "Missal" e "Broquéis" de Cruz e Sousa?',
+    ' O Simbolismo'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'ingles',
+    Como se diz "livro" e "caneta" em inglês?',
+    'Book e pen.'
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'ingles,
+    'Qual é o passado do verbo irregular to go?',
+    'Went.
 )
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'filisofia',
+    'A quem é atribuída a famosa frase "Só sei que nada sei"?',
+    'Sócrates'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'filisofia,
+    'O que estuda a Ética dentro da filosofia?',
+    'Os princípios que guiam o comportamento humano, diferenciando o que é bom, justo ou correto.
 )
 
 criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'sociologia',
+    'Quem é considerado o "pai" da sociologia e criador do Positivismo?',
+    'Resposta: Auguste Comte.'
 )
 
 criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'sociologia',
+    'O que Karl Marx definia como o motor da história humana?',
+    'A luta de classes.'
 )
 
 criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
-)
-
-criaCartao(
-    'Disciplina 3',
-    'Pergunta 3',
-    'Resposta 3'
-)
-
-criaCartao(
-    'Disciplina 1',
-    'Pergunta 1',
-    'Resposta 1'
-)
-
-criaCartao(
-    'Disciplina 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'artes',
+    'Quem pintou o famoso quadro "Abaporu", símbolo do Modernismo brasileiro?',
+    'Tarsila do Amaral.'
 )
 
 criaCartao(
